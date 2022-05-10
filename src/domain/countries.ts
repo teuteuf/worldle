6cb3fe0f2cf41e3d8435deddd37fa268aab70614
Bartosz <bartoszsdr@gmail.com>
2022-05-10 14:36:59 +0200

    fixed deploy errors

--- src/domain/countries.ts
+++ src/domain/countries.ts
@@ -3,49 +3,45 @@
 // Countries images => https://github.com/djaiss/mapsicon
 // Country area => https://github.com/samayo/country-json/blob/master/src/country-by-surface-area.json
 
-import { areas } from "./countries.area";
-import { countries } from "./countries.position";
-import { frenchCountryNames } from "./countries.name.fr";
-import { hungarianCountryNames } from "./countries.name.hu";
-import { dutchCountryNames } from "./countries.name.nl";
-import { polishCountryNames } from "./countries.name.pl";
-import { countryCodesWithImage } from "./countries.image";
+import { areas } from './countries.area'
+import { countries } from './countries.position'
+import { frenchCountryNames } from './countries.name.fr'
+import { hungarianCountryNames } from './countries.name.hu'
+import { dutchCountryNames } from './countries.name.nl'
+import { polishCountryNames } from './countries.name.pl'
+import { countryCodesWithImage } from './countries.image'
 
 export interface Country {
-  code: string;
-  latitude: number;
-  longitude: number;
-  name: string;
+	code: string
+	latitude: number
+	longitude: number
+	name: string
 }
 
-export const countriesWithImage = countries.filter((c) =>
-  countryCodesWithImage.includes(c.code.toLowerCase())
-);
+export const countriesWithImage = countries.filter(c => countryCodesWithImage.includes(c.code.toLowerCase()))
 
-export const smallCountryLimit = 5000;
-export const bigEnoughCountriesWithImage = countriesWithImage.filter(
-  (country) => areas[country.code] > smallCountryLimit
-);
+export const smallCountryLimit = 5000
+export const bigEnoughCountriesWithImage = countriesWithImage.filter(country => areas[country.code] > smallCountryLimit)
 
 export function getCountryName(language: string, country: Country) {
-  switch (language) {
-    case "fr":
-      return frenchCountryNames[country.code];
-    case "hu":
-      return hungarianCountryNames[country.code];
-    case "nl":
-      return dutchCountryNames[country.code];
-      case "pl":
-      return polishCountryNames[country.code];
-    default:
-      return country.name;
-  }
+	switch (language) {
+		case 'fr':
+			return frenchCountryNames[country.code]
+		case 'hu':
+			return hungarianCountryNames[country.code]
+		case 'nl':
+			return dutchCountryNames[country.code]
+		case 'pl':
+			return polishCountryNames[country.code]
+		default:
+			return country.name
+	}
 }
 
 export function sanitizeCountryName(countryName: string): string {
-  return countryName
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[- '()]/g, "")
-    .toLowerCase();
+	return countryName
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.replace(/[- '()]/g, '')
+		.toLowerCase()
 }
